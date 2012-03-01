ab1f7e9492b7e85598bf0a9c2fdc269fe3f2a56c
Joe Rozner <joe@deadbytes.net>
2012-02-29 22:47:43 -0800

    flush ruleset before adding and fix loops

--- bin/linux_firewall.sh
+++ bin/linux_firewall.sh
@@ -4,39 +4,42 @@
 # order to work. Specify TCP and UDP ports in their respective vars in a
 # space separated list.
 
-IPTABLES=$(which iptables)
-TCP=""
-UDP=""
+iptables=$(which iptables)
+tcp=""
+udp=""
 
 if [ $(id -u) -ne 0 ]; then
   echo "Re-run as root to set firewall."
   exit 1
 fi
 
+# Flush current rule set to start over
+$iptables -F
+
 # Set default policy for all chains to DROP
-$IPTABLES INPUT DROP
-$IPTABLES FORWARD DROP
-$IPTABLES OUTPUT DROP
+$iptables INPUT DROP
+$iptables FORWARD DROP
+$iptables OUTPUT DROP
 
 # Make sure localhost is really "localhost"
-$IPTABLES -A INPUT -i lo -j ACCEPT
-$IPTABLES -A INPUT -s 127.0.0.0/8 -j DROP
+$iptables -A INPUT -i lo -j ACCEPT
+$iptables -A INPUT -s 127.0.0.0/8 -j DROP
 
 # Accept everything that's already established
-$IPTABLES -A INPUT -p tcp -m state --state ESTABLISHED -j ACCEPT
-$IPTABLES -A INPUT -p udp -m state --state ESTABLISHED -j ACCEPT
+$iptables -A INPUT -p tcp -m state --state ESTABLISHED -j ACCEPT
+$iptables -A INPUT -p udp -m state --state ESTABLISHED -j ACCEPT
 
 # Allow specified TCP and UDP ports
-for $PORT in $TCP; do
-  $IPTABLES -A INPUT -p tcp -m tcp --dport $PORT -m state --state NEW -j ACCEPT
+for port in $tcp; do
+  $iptables -A INPUT -p tcp -m tcp --dport $port -m state --state NEW -j ACCEPT
 done
 
-for $PORT in $UDP; do
-  $IPTABLES -A INPUT -p udp -m udp --dport $PORT -m state --state NEW -j ACCEPT
+for port in $udp; do
+  $iptables -A INPUT -p udp -m udp --dport $port -m state --state NEW -j ACCEPT
 done
 
 # Allow all outbound packets
-$IPTABLES -A OUTPUT -o lo -j ACCEPT
-$IPTABLES -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED -j ACCEPT
-$IPTABLES -A OUTPUT -p udp -m state --state NEW,ESTABLISHED -j ACCEPT
-$IPTABLES -A OUTPUT -p icmp -m state --state NEW,ESTABLISHED -j ACCEPT
+$iptables -A OUTPUT -o lo -j ACCEPT
+$iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED -j ACCEPT
+$iptables -A OUTPUT -p udp -m state --state NEW,ESTABLISHED -j ACCEPT
+$iptables -A OUTPUT -p icmp -m state --state NEW,ESTABLISHED -j ACCEPT
